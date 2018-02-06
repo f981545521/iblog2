4bcba7d304e8443b2c0575c0ecba56186256a5c2
f981545521 <981545521@qq.com>
2018-02-06 21:07:03 +0800

    工程重构

--- iblog-test/src/main/java/extendtest/ExtendsTest.java
+++ iblog-test/src/main/java/extendtest/ExtendsTest.java
@@ -1,10 +1,17 @@
 package extendtest;
 
+import org.junit.Test;
+
 /**
  * @author youfang
  * @date 2018-02-06 18:20
  **/
 public class ExtendsTest {
+
+    @Test
+    public void test1(){
+        System.out.println("gogo simida");
+    }
 
 
 
