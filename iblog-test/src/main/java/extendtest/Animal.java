32b07d97b651513e60f4b2ba3f9ba07173f6599c
youfang <981545521@qq.com>
2018-02-06 16:58:36 +0800

    add Test

---
+++ iblog-test/src/main/java/extendtest/Animal.java
@@ -0,0 +1,12 @@
+package extendtest;
+
+/**
+ * @author youfang
+ * @date 2018-02-06 16:53
+ **/
+public class Animal{
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.wangwang();
+    }
+}
