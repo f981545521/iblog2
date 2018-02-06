7eaccdbaf383a7cdb65e4eed6628a7b67210e710
youfang <981545521@qq.com>
2018-02-06 16:58:36 +0800

    add Test

---
+++ iblog-test/src/main/java/extendtest/Dog.java
@@ -0,0 +1,12 @@
+package extendtest;
+
+/**
+ * @author youfang
+ * @date 2018-02-06 16:53
+ **/
+public class Dog extends Animal{
+
+    public void wangwang(){
+        System.out.println("旺旺！");
+    }
+}
