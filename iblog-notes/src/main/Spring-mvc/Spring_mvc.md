0d10ad68dca23a05df73ef1059a4636c16b8bb85
youfang <981545521@qq.com>
2018-02-08 18:29:22 +0800

    add jackson

---
+++ iblog-notes/src/main/Spring-mvc/Spring_mvc.md
@@ -0,0 +1,8 @@
+### Spring
+
+#### @ResponseBody
+
+Spring mvc 中的 @ResponseBody注解用来返回JSON，使用了JackSon转换，需要导入JackSon的Jar包
+
+
+
--- iblog-web/src/main/java/cn/acyou/iblog/controller/DemoController.java
+++ iblog-web/src/main/java/cn/acyou/iblog/controller/DemoController.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
  * @date 2017年8月29日 下午2:43:49
  */
 @Controller
-@RequestMapping("demo")
+@RequestMapping("/demo")
 public class DemoController {
 
     /**
@@ -30,20 +30,22 @@ public class DemoController {
      * getParameter表示接收参数，参数为页面提交的参数.
      * 因此这个并没有设置参数的方法（没有setParameter），而且接收参数返回的不是Object，而是String类型
      */
-    @RequestMapping("gettest.do")
+    @RequestMapping("/gettest.do")
     @ResponseBody
     public JsonResult demo1(String username, String password, HttpServletRequest request){
         System.out.println("表单输入："+username+","+password);
         request.setAttribute("username", "我就是666");
-
         Object s = request.getAttribute("username");
         System.out.println("getAttribute:"+s);
         String ss = request.getParameter("username");//对应input中的name属性
         System.out.println("getParameter:"+ss);
-
-
         return new JsonResult("测试代码");
+    }
 
+    @RequestMapping(value = "/getXml.do",produces = {"application/xml; charset=UTF-8"})
+    @ResponseBody
+    public JsonResult demo1(){
+        return new JsonResult("测试代码");
     }
 
 
