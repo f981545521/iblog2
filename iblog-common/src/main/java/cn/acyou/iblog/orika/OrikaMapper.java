383126e977e84f2cff00b4aa5171deac256dbc5d
youfang <981545521@qq.com>
2018-02-09 17:31:53 +0800

    add OrikaMapper

---
+++ iblog-common/src/main/java/cn/acyou/iblog/orika/OrikaMapper.java
@@ -0,0 +1,51 @@
+package cn.acyou.iblog.orika;
+
+import cn.acyou.iblog.orika.converter.BigIntegerConverter;
+import com.google.common.collect.Lists;
+import ma.glasnost.orika.MapperFacade;
+import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.impl.DefaultMapperFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.PostConstruct;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author youfang
+ * @date 2018-02-09 17:11
+ **/
+public class OrikaMapper {
+    private MapperFacade mapperFacade = null;
+
+    private MapperFactory mapperFactory = new DefaultMapperFactory.Builder().build();
+    @Autowired(required = false)
+    private List<OrikaRegisty> orikaRegistyList = Lists.newArrayList();
+
+    /**
+     * 初始化Orika注册
+     */
+    @PostConstruct
+    private void init() {
+        if (!orikaRegistyList.isEmpty()) {
+            for (OrikaRegisty orikaRegisty : orikaRegistyList) {
+                orikaRegisty.register(mapperFactory);
+            }
+        }
+        mapperFactory.getConverterFactory().registerConverter(BigIntegerConverter.class.getSimpleName(), new BigIntegerConverter());
+        mapperFacade = mapperFactory.getMapperFacade();
+    }
+
+
+    public <V, P> P convert(V base, Class<P> target) {
+        return mapperFacade.map(base, target);
+    }
+
+    public <V, P> List<P> convertList(List<V> baseList, Class<P> target) {
+        List<P> list = baseList.isEmpty() ? Collections.<P>emptyList() : mapperFacade.mapAsList(baseList, target);
+        return list;
+    }
+
+
+
+}
--- iblog-web/src/test/java/cn/acyou/iblog/excutortest/ExcutorTest.java
+++ iblog-web/src/test/java/cn/acyou/iblog/excutortest/ExcutorTest.java
@@ -1,13 +1,17 @@
-package cn.acyou.iblog.maintest;
+package cn.acyou.iblog.excutortest;
 
+import cn.acyou.iblog.entity.People;
+import cn.acyou.iblog.entity.Teacher;
 import cn.acyou.iblog.executor.AfterCommitExecutor;
+import cn.acyou.iblog.maintest.BaseTest;
+import cn.acyou.iblog.orika.OrikaMapper;
 import org.junit.Test;
 
 /**
  * @author youfang
  * @date 2018-02-09 12:48
  **/
-public class ExcutorTest extends BaseTest{
+public class ExcutorTest extends BaseTest {
 
     @Test
     public void test1(){
@@ -29,7 +33,16 @@ public class ExcutorTest extends BaseTest{
         });
         log.info("执行结束！！");
         System.out.println(executor);
+    }
 
 
+    @Test
+    public void testOrikaMapper(){
+        OrikaMapper orikaMapper = ctx.getBean("orikaMapper", OrikaMapper.class);
+        People people = new People(1,"张飞",23);
+        Teacher teacher = orikaMapper.convert(people,Teacher.class);
+        System.out.println(teacher.getAddress() == null);
+        System.out.println(people);
+        System.out.println(teacher);
     }
 }
