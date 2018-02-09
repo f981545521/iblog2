ed4f61772f7e487e52dd621901bd76621fc13b0a
youfang <981545521@qq.com>
2018-02-09 11:32:02 +0800

    add Jsp support

---
+++ iblog-common/src/main/java/cn/acyou/iblog/constant/AppConstant.java
@@ -0,0 +1,12 @@
+package cn.acyou.iblog.constant;
+
+/**
+ * @author youfang
+ * @date 2018-02-09 11:08
+ **/
+public class AppConstant {
+
+    public static final String SHORT_DATE_PATTERN = "yyyyMMdd";
+    public static final String DEFAULT_DATE_FORMAT_PATTERN = "yyyy-MM-dd";
+    public static final String SPECIFIC_DATE_FORMAT_PATTERN = "yyyy-MM-dd HH:mm:ss";
+}
