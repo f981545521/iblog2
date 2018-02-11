e6770b924daf6fcb7f7261f62e5ac342c2565468
youfang <981545521@qq.com>
2018-02-11 09:28:49 +0800

    update : 为什么要加repository注解才可以？

--- iblog-web/src/main/java/cn/acyou/iblog/controller/HelloController.java
+++ iblog-web/src/main/java/cn/acyou/iblog/controller/HelloController.java
@@ -1,8 +1,13 @@
 package cn.acyou.iblog.controller;
 
+import cn.acyou.iblog.model.test.TBoss;
+import cn.acyou.iblog.service.TBossService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * @author youfang
@@ -12,10 +17,14 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/hello")
 public class HelloController extends BaseController{
 
+    @Autowired
+    private TBossService tBossService;
+
     @RequestMapping("/hellojsp")
     public ModelAndView getHelloPage(){
         ModelAndView mv = new ModelAndView();
         mv.setViewName("/hello/hello");
+        List<TBoss> tBossList = tBossService.getAllBoss();
         return  mv;
     }
 }
