1e11110511c38180c880635878ccb75293ce1d65
youfang <981545521@qq.com>
2018-02-28 13:29:12 +0800

    add ：redis service

--- iblog-web/src/test/java/cn/acyou/iblog/utiltytest/RedisUtilTest.java
+++ iblog-web/src/test/java/cn/acyou/iblog/utiltytest/RedisUtilTest.java
@@ -1,18 +1,27 @@
 package cn.acyou.iblog.utiltytest;
 
-import cn.acyou.iblog.utility.RedisUtil;
+import cn.acyou.iblog.maintest.BaseTest;
+import cn.acyou.iblog.redis.RedisUtil;
 import org.junit.Test;
+import org.springframework.data.redis.core.RedisTemplate;
 import redis.clients.jedis.Jedis;
 
 /**
  * @author youfang
  * @create 2018-02-08 22:42
  */
-public class RedisUtilTest {
+public class RedisUtilTest extends BaseTest{
 
     @Test
-    public void test1(){
-        Jedis jedis = RedisUtil.getJedis();
-        System.out.println(jedis);
+    public void testJedis() {
+        RedisTemplate redisTemplate = applicationContext.getBean("redisTemplate", RedisTemplate.class);
+        System.out.println(redisTemplate);
+    }
+    @Test
+    public void testRedisUtil() {
+        RedisUtil redisTemplate = applicationContext.getBean("redisUtil", RedisUtil.class);
+        redisTemplate.cacheValue("first","F1");
+        System.out.println(redisTemplate.getValue("first"));
+        System.out.println(redisTemplate.containsValueKey("first"));
     }
 }
