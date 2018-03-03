3ac482ed09fe06d4bdf97fd967c8b3998d4d2357
f981545521 <981545521@qq.com>
2018-03-03 12:13:04 +0800

    add : abstract class test

---
+++ iblog-test/src/main/java/extendtest/Wolf.java
@@ -0,0 +1,18 @@
+package extendtest;
+
+/**
+ * @author youfang
+ * @date 2018-03-03 12:10
+ */
+public class Wolf extends Zoo{
+    @Override
+    public void sleep() {
+        System.out.println("狼晚上不睡觉");
+    }
+
+    public static void main(String[] args) {
+        Zoo zoo = new Wolf();
+        zoo.eat();
+        zoo.sleep();
+    }
+}
---
+++ iblog-test/src/main/java/extendtest/Zoo.java
@@ -0,0 +1,14 @@
+package extendtest;
+
+/**
+ * @author youfang
+ * @date 2018-03-03 12:08
+ */
+public abstract class Zoo {
+
+    public void eat() {
+        System.out.println("动物园饲养动物");
+    }
+
+    public abstract void sleep();
+}
