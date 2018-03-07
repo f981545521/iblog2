8411519baef7eadc6838ba01c387e309ffb8bb5f
youfang <981545521@qq.com>
2018-03-08 01:43:20 +0800

    add :  test case

---
+++ iblog-test/src/main/java/threadtest/TestSync2.java
@@ -0,0 +1,38 @@
+package threadtest;
+
+/**
+ * @author youfang
+ * @date 2018-03-08 1:35
+ **/
+public class TestSync2 implements Runnable {
+    int b = 100;
+
+    synchronized void m1() throws InterruptedException {
+        b = 1000;
+        Thread.sleep(500); //6
+        System.out.println("b=" + b);
+    }
+
+    synchronized void m2() throws InterruptedException {
+        Thread.sleep(250); //5
+        b = 2000;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        TestSync2 tt = new TestSync2();
+        Thread t = new Thread(tt);  //1
+        t.start(); //2
+        tt.m2(); //3
+        System.out.println("main thread b=" + tt.b); //4
+    }
+
+    @Override
+    public void run() {
+        try {
+            m1();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
