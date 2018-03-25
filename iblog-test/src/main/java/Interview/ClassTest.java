786b2eae1336297fcac19c01764a279b32d44955
youfang <981545521@qq.com>
2018-03-25 12:06:02 +0800

    add : interview summary

--- iblog-test/src/main/java/Interview/ClassTest.java
+++ iblog-test/src/main/java/Interview/ClassTest.java
@@ -4,6 +4,8 @@ package Interview;
  * @author youfang
  * @date 2018-03-24 下午 07:47
  * 一个文件中可以存在多个类。但是只能有一个public类，并且此public类必须与文件名相同
+ * 编译的时候内部类会编译成：ClassTest$Foo.class 匿名内部类：ClassTest$1.class
+ * Aoo Boo 则会编译成独立的class文件。
  **/
 public class ClassTest {
 
