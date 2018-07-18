77183144aa9a7d084579a225544f39e2ec827136
youfang <981545521@qq.com>
2018-07-18 12:10:04 +0800

    add : time test

--- iblog-test/src/main/java/jodatimetest/JoDaTimeTest.java
+++ iblog-test/src/main/java/jodatimetest/JoDaTimeTest.java
@@ -3,9 +3,12 @@ package jodatimetest;
 import org.joda.time.DateTime;
 import org.joda.time.Days;
 import org.joda.time.Years;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.junit.Test;
 
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * 参考文章：https://www.ibm.com/developerworks/cn/java/j-jodatime.html
@@ -60,5 +63,49 @@ public class JoDaTimeTest {
         int diffDay = Days.daysBetween(birthDay, nowDay).getDays();
         System.out.println(diffYear);
         System.out.println(diffDay);
+    }
+
+    @Test
+    public void test23(){
+        String dateStr = "2018-07-18 12:23:23";
+        DateTime dateTime = new DateTime().minusDays(1);
+        System.out.println(dateTime.toString("yyyy-MM-dd HH:mm:ss"));
+        DateTimeFormatter format = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+        DateTime yesterDay = DateTime.parse(dateStr, format);
+        System.out.println(yesterDay.toLocalDate().equals(dateTime.toLocalDate()));
+
+    }
+
+
+
+
+    @Test
+    public void testFormat() {
+
+        System.out.println(format(new Date()));     //今天
+
+        System.out.println(format(new DateTime(new Date()).minusDays(1).toDate())); //昨天
+
+        System.out.println(format(new DateTime(new Date()).minusDays(2).toDate())); //2天前
+
+        System.out.println(format(new DateTime(new Date()).plusDays(1).toDate())); //1天后
+    }
+
+    private String format(Date date) {
+        DateTime now = new DateTime();
+        DateTime today_start = new DateTime(now.getYear(), now.getMonthOfYear(), now.getDayOfMonth(), 0, 0, 0);
+        DateTime today_end = today_start.plusDays(1);
+        DateTime yesterday_start = today_start.minusDays(1);
+
+        if(date.after(today_start.toDate()) && date.before(today_end.toDate())) {
+            return String.format("今天 %s", new DateTime(date).toString("HH:mm"));
+        } else if(date.after(yesterday_start.toDate()) && date.before(today_start.toDate())) {
+            return String.format("昨天 %s", new DateTime(date).toString("HH:mm"));
+        }
+
+        return new DateTime(date).toString("yyyy-MM-dd HH:mm");
+
+
+
     }
 }
