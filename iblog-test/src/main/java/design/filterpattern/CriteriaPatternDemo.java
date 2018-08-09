b2942023af60d4e7e2d2587493225205d8ef76c0
youfang <981545521@qq.com>
2018-08-09 21:36:40 +0800

    add : filter design pattern

---
+++ iblog-test/src/main/java/design/filterpattern/CriteriaPatternDemo.java
@@ -0,0 +1,50 @@
+package design.filterpattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 过滤器模式（Filter Pattern）或标准模式（Criteria Pattern）是一种设计模式，
+ * 这种模式允许开发人员使用不同的标准来过滤一组对象，通过逻辑运算以解耦的方式把它们连接起来。这种类型的设计模式属于结构型模式，它结合多个标准来获得单一标准。
+ * @author youfang
+ * @version [1.0.0, 2018-08-09 下午 09:25]
+ **/
+public class CriteriaPatternDemo {
+    public static void main(String[] args) {
+        List<Person> persons = new ArrayList<Person>();
+
+        persons.add(new Person("Robert","Male", "Single"));
+        persons.add(new Person("John","Male", "Married"));
+        persons.add(new Person("Laura","Female", "Married"));
+        persons.add(new Person("Diana","Female", "Single"));
+        persons.add(new Person("Mike","Male", "Single"));
+        persons.add(new Person("Bobby","Male", "Single"));
+
+        Criteria male = new CriteriaMale();
+        Criteria female = new CriteriaFemale();
+        Criteria single = new CriteriaSingle();
+        Criteria singleMale = new AndCriteria(single, male);
+        Criteria singleOrFemale = new OrCriteria(single, female);
+
+        System.out.println("Males: ");
+        printPersons(male.meetCriteria(persons));
+
+        System.out.println("\nFemales: ");
+        printPersons(female.meetCriteria(persons));
+
+        System.out.println("\nSingle Males: ");
+        printPersons(singleMale.meetCriteria(persons));
+
+        System.out.println("\nSingle Or Females: ");
+        printPersons(singleOrFemale.meetCriteria(persons));
+    }
+
+    public static void printPersons(List<Person> persons){
+        for (Person person : persons) {
+            System.out.println("Person : [ Name : " + person.getName()
+                    +", Gender : " + person.getGender()
+                    +", Marital Status : " + person.getMaritalStatus()
+                    +" ]");
+        }
+    }
+}
---
+++ iblog-test/src/main/java/design/filterpattern/OrCriteria.java
@@ -0,0 +1,31 @@
+package design.filterpattern;
+
+import java.util.List;
+
+/**
+ * @author youfang
+ * @version [1.0.0, 2018-08-09 下午 09:25]
+ **/
+public class OrCriteria implements Criteria {
+
+    private Criteria criteria;
+    private Criteria otherCriteria;
+
+    public OrCriteria(Criteria criteria, Criteria otherCriteria) {
+        this.criteria = criteria;
+        this.otherCriteria = otherCriteria;
+    }
+
+    @Override
+    public List<Person> meetCriteria(List<Person> persons) {
+        List<Person> firstCriteriaItems = criteria.meetCriteria(persons);
+        List<Person> otherCriteriaItems = otherCriteria.meetCriteria(persons);
+
+        for (Person person : otherCriteriaItems) {
+            if(!firstCriteriaItems.contains(person)){
+                firstCriteriaItems.add(person);
+            }
+        }
+        return firstCriteriaItems;
+    }
+}
