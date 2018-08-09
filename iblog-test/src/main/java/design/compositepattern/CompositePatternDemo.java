e53f12489d907d725d8742b0af5dba67d238069a
youfang <981545521@qq.com>
2018-08-09 22:58:23 +0800

    add : Composite design pattern

---
+++ iblog-test/src/main/java/design/compositepattern/CompositePatternDemo.java
@@ -0,0 +1,47 @@
+package design.compositepattern;
+
+/**
+ * 组合模式（Composite Pattern），又叫部分整体模式，是用于把一组相似的对象当作一个单一的对象。组合模式依据树形结构来组合对象，用来表示部分以及整体层次。这种类型的设计模式属于结构型模式，它创建了对象组的树形结构。
+ *
+ * 这种模式创建了一个包含自己对象组的类。该类提供了修改相同对象组的方式。
+ *
+ * 意图：将对象组合成树形结构以表示"部分-整体"的层次结构。组合模式使得用户对单个对象和组合对象的使用具有一致性。
+ * 主要解决：它在我们树型结构的问题中，模糊了简单元素和复杂元素的概念，客户程序可以向处理简单元素一样来处理复杂元素，从而使得客户程序与复杂元素的内部结构解耦。
+ * 何时使用： 1、您想表示对象的部分-整体层次结构（树形结构）。 2、您希望用户忽略组合对象与单个对象的不同，用户将统一地使用组合结构中的所有对象。
+ * 如何解决：树枝和叶子实现统一接口，树枝内部组合该接口。
+ * 关键代码：树枝内部组合该接口，并且含有内部属性 List，里面放 Component。
+ * @author youfang
+ * @version [1.0.0, 2018-08-09 下午 10:55]
+ **/
+public class CompositePatternDemo {
+    public static void main(String[] args) {
+        Employee CEO = new Employee("John","CEO", 30000);
+
+        Employee headSales = new Employee("Robert","Head Sales", 20000);
+        Employee headMarketing = new Employee("Michel","Head Marketing", 20000);
+
+        Employee clerk1 = new Employee("Laura","Marketing", 10000);
+        Employee clerk2 = new Employee("Bob","Marketing", 10000);
+
+        Employee salesExecutive1 = new Employee("Richard","Sales", 10000);
+        Employee salesExecutive2 = new Employee("Rob","Sales", 10000);
+
+        CEO.add(headSales);
+        CEO.add(headMarketing);
+
+        headSales.add(salesExecutive1);
+        headSales.add(salesExecutive2);
+
+        headMarketing.add(clerk1);
+        headMarketing.add(clerk2);
+
+        //打印该组织的所有员工
+        System.out.println(CEO);
+        for (Employee headEmployee : CEO.getSubordinates()) {
+            System.out.println(headEmployee);
+            for (Employee employee : headEmployee.getSubordinates()) {
+                System.out.println(employee);
+            }
+        }
+    }
+}
---
+++ iblog-test/src/main/java/design/compositepattern/Employee.java
@@ -0,0 +1,41 @@
+package design.compositepattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author youfang
+ * @version [1.0.0, 2018-08-09 下午 10:55]
+ **/
+public class Employee {
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    //构造函数
+    public Employee(String name,String dept, int sal) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = sal;
+        subordinates = new ArrayList<Employee>();
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates(){
+        return subordinates;
+    }
+
+    public String toString(){
+        return ("Employee :[ Name : "+ name
+                +", dept : "+ dept + ", salary :"
+                + salary+" ]");
+    }
+}
