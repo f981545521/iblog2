5a78cdc7fe26bd82c28398e694d82f50bdb28045
youfang <981545521@qq.com>
2018-08-09 09:47:38 +0800

    add : test

--- iblog-test/src/main/java/messytest/MessyTest2.java
+++ iblog-test/src/main/java/messytest/MessyTest2.java
@@ -1,7 +1,9 @@
 package messytest;
 
+import com.google.common.collect.Lists;
 import org.junit.Test;
 
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -50,5 +52,17 @@ public class MessyTest2 {
         Scanner scan = new Scanner(System.in);
         int i = scan.nextInt();
         System.out.println(i);
+    }
+
+
+    @Test
+    public void test4(){
+        List<String> strings = Lists.newArrayList();
+        strings.add("aaa");
+        strings.add("bbb");
+        strings.add("ccc");
+        System.out.println(strings);
+        strings.clear();
+        System.out.println(strings);
     }
 }
