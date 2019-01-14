7df13b73cfefa186dbaf37d73a38c9a9bad9b15b
youfang <981545521@qq.com>
2019-01-14 09:37:08 +0800

    add : framework test

---
+++ iblog-test/src/main/java/frameworktest/FrameWorkTest.java
@@ -0,0 +1,19 @@
+package frameworktest;
+
+import org.junit.Test;
+
+/**
+ * @author youfang
+ * @version [1.0.0, 2019-01-04 下午 02:11]
+ **/
+public class FrameWorkTest {
+
+    @Test
+    public void test1(){
+
+    }
+
+    public String reStr(String code){
+        return "输出的是：" + code;
+    }
+}
