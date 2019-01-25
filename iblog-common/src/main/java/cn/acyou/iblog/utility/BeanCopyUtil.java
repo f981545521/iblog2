ce31cd2b8af63feb5323e3d023a561d68a72b93a
youfang <981545521@qq.com>
2019-01-25 20:35:21 +0800

    add : util

---
+++ iblog-common/src/main/java/cn/acyou/iblog/utility/BeanCopyUtil.java
@@ -0,0 +1,33 @@
+package cn.acyou.iblog.utility;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author JianJun.Li
+ * @date 2018/10/12 10:35
+ **/
+public class BeanCopyUtil {
+
+    public static <T,E> E copy(T t,Class<E> clz){
+        try {
+            E e = clz.newInstance();
+            BeanUtils.copyProperties(t,e);
+            return e;
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+        return null;
+    }
+
+    public static <T,E> List<E> copyList(List<T> l,Class<E> clz){
+        List<E> list = new ArrayList<>();
+        if(!CollectionUtils.isEmpty(l)){
+            l.forEach(item->list.add(copy(item,clz)));
+        }
+        return list;
+    }
+}
